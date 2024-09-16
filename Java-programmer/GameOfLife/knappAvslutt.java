acfc586ab1316fe5984f12d0f13fc46e863ef890
Adrian0Lund <adix0905@gmail.com>
2024-09-16 10:02:40 +0200

    Add files via upload

---
+++ Java-programmer/GameOfLife/knappAvslutt.java
@@ -0,0 +1,11 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+class knappAvslutt implements ActionListener {
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        System.exit(0);
+    }
+    
+}
